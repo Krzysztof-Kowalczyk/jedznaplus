13d02e74351579504f76a00bdb09edd70e30d6e5
kalik <wargas_14@o2.pl>
2014-10-15 23:10:09 +0200

    1

---
+++ Jedznaplus/Models/Recipe.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Jedznaplus.Models
+{
+    public class Recipe
+    {
+        public long Id { get; set; }
+        public string Name { get; set; }
+        public string Ingredients { get; set; }
+        public string PreparationMethod { get; set; }
+        public string ImageUrl { get; set; }
+        public int PreparationTime { get; set; }
+        public int CookTime { get; set; }
+        public int TotalTime { get; set; }
+        public int Serves { get; set; }
+        public int Calories { get; set; }
+        public string UserName { get; set; }
+
+    }
+}

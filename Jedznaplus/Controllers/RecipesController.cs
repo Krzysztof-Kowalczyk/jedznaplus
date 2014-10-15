40650c5b4443ae52e2a1cfe09787db47cb50d42c
kalik <wargas_14@o2.pl>
2014-10-15 23:18:38 +0200

    1

---
+++ Jedznaplus/Controllers/RecipesController.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Jedznaplus.Controllers
+{
+    public class RecipesController : Controller
+    {
+        //
+        // GET: /Recipes/
+
+        public ActionResult Index()
+        {
+
+            var auction = new []{
+                new Models.Recipe(){
+                Name = "Pomidorowka",
+                Ingredients="Koncentrat pomidorowy, rosół z niedzieli, śmietana, cukier",
+                PreparationMethod = "Podgrzej rosół, wlej koncentrat, dodaj troche cukru, dodaj śmietany",
+                PreparationTime = 0,
+                CookTime = 10,
+                TotalTime = 10,
+                Serves = 4,
+                Calories=200,
+                UserName="Krzysztof"
+                },
+                new Models.Recipe(){
+                Name = "Pomidorowka",
+                Ingredients="Koncentrat pomidorowy, rosół z niedzieli, śmietana, cukier",
+                PreparationMethod = "Podgrzej rosół, wlej koncentrat, dodaj troche cukru, dodaj śmietany",
+                PreparationTime = 0,
+                CookTime = 10,
+                TotalTime = 10,
+                Serves = 4,
+                Calories=200,
+                UserName="Krzysztof"
+                }
+            };
+
+            return View(auction);
+        }
+
+    }
+}

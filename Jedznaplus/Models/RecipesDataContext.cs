94844628a45116ba78b8948609590e00e54f11cb
kalik <wargas_14@o2.pl>
2014-10-16 22:02:08 +0200

    1

---
+++ Jedznaplus/Models/RecipesDataContext.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Web;
+
+namespace Jedznaplus.Models
+{
+    public class RecipesDataContext : DbContext
+    {
+        public DbSet<Recipe> Recpes { get; set; }
+    }
+}
